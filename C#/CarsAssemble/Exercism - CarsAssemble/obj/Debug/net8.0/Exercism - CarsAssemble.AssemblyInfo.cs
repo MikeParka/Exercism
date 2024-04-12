ae4b3f3b56e76873dd35406dff684a1d6f6e1878
MikeParka <39887424+MikeParka@users.noreply.github.com>
2024-04-12 16:10:35 +0100

    ...

--- C#/CarsAssemble/Exercism - CarsAssemble/obj/Debug/net8.0/Exercism - CarsAssemble.AssemblyInfo.cs
+++ C#/CarsAssemble/Exercism - CarsAssemble/obj/Debug/net8.0/Exercism - CarsAssemble.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercism - CarsAssemble")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9989bf3f826aed478829a67c02c759c5733ddb6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c154177164e3e46ee4c66c9acb8dc3908c1272de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercism - CarsAssemble")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercism - CarsAssemble")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
